a254dc48691ac64dd8204143bbdcbf1d7209fee7
Darji Het <hetdarjibca22@gmail.com>
2025-08-13 16:44:27 +0530

    commit

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,9 +5,14 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["*"] // or just skip origin to allow same-origin requests
+    : ["http://localhost:5173"];
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: allowedOrigins,
   },
 });
 
